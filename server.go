4ebbb239989db32b011126e1ce64117e020cd695
freddedotme-v2 <code@fredde.me>
2025-01-07 21:01:02 +0100

    adding client and server structs + go routines + using json format for messages

--- server.go
+++ server.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
+
+type Server struct {
+	clients []Client
+	channel chan []byte
+}
+
+type Client struct {
+	server *Server
+	conn   *websocket.Conn
+}
+
+type Message struct {
+	Name string
+	Body string
+}
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -13,22 +29,19 @@ var upgrader = websocket.Upgrader{
 }
 
 func main() {
+	server := &Server{clients: make([]Client, 0), channel: make(chan []byte)}
+	go server.run()
+
 	http.HandleFunc("/listen", func(w http.ResponseWriter, r *http.Request) {
 		conn, _ := upgrader.Upgrade(w, r, nil)
 
-		// Listen for incoming messages.
-		for {
-			msgType, msg, err := conn.ReadMessage()
-			if err != nil {
-				return
-			}
+		client := Client{server: server, conn: conn}
+		server.clients = append(server.clients, client)
 
-			fmt.Printf("%s: %s\n", conn.RemoteAddr(), msg)
+		fmt.Println("Status: New client connected")
 
-			if err = conn.WriteMessage(msgType, msg); err != nil {
-				return
-			}
-		}
+		// Spinning up a new routine for each new client.
+		go client.run()
 	})
 
 	http.HandleFunc("/app", func(w http.ResponseWriter, r *http.Request) {
@@ -36,4 +49,39 @@ func main() {
 	})
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func (s *Server) run() {
+	for {
+		msg := <-s.channel
+
+		var message Message
+		err := json.Unmarshal(msg, &message)
+
+		if err != nil {
+			return
+		}
+
+		for _, client := range s.clients {
+			if err := client.conn.WriteMessage(1, []byte(fmt.Sprintf("%s: %s", message.Name, message.Body))); err != nil {
+				continue
+			}
+		}
+	}
+}
+
+func (c *Client) run() {
+	defer func() {
+		c.conn.Close()
+		fmt.Println("Status: Client disconnected")
+	}()
+
+	for {
+		_, msg, err := c.conn.ReadMessage()
+		if err != nil {
+			return
+		}
+
+		c.server.channel <- msg
+	}
 }
